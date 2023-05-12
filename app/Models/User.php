de774d881fb30dd20ef7dedfecb0411789627aa6
AMOUCHAL Moustapha <amouchaldev@gmail.com>
2023-05-12 14:52:11 +0100

    jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Models;
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,11 +43,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-
-    public function reservations() {
-        return $this->hasMany(Reservation::class);
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
     }
-    public function hotels() {
-        return $this->hasMany(Hotel::class);
+    
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\AuthController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -134,8 +138,42 @@ Route::apiResources([
 
 
 
-Route::get('/test', function () {
-    return Room::whereHas('offer')->with('offer')->get();
+
+
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/', function() {
+    $data = [
+        'message' => "Welcome to our API"
+    ];
+    return response()->json($data, 200);
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/user', [AuthController::class, 'getUser']);
 
+
+Route::middleware('jwt.verify')->group(function() {
+    Route::get('/test', function () {
+        return response()->json(['test' => 'test test']);
+    });
+    
+    Route::get('/dashboard', function() {
+        return response()->json(['message' => 'Welcome to dashboard'], 200);
+    });
+});
