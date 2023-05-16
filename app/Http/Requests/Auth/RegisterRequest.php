0c755d4def8005b3863064003d1410e72efc7cbc
AMOUCHAL Moustapha <amouchaldev@gmail.com>
2023-05-16 16:31:12 +0100

    add http code to login, register request

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -51,6 +51,6 @@ class RegisterRequest extends FormRequest
             'success' => false,
             'message' => 'Validation errors',
             'data' => $validator->errors()
-        ]));
+        ], 422));
     }
 }
